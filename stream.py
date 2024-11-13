38fe30e50d04b5b5e12812590e404ad17052e347
flaret <flakiee@derpy.pink>
2024-11-13 16:44:51 +0800

    v6

--- stream.py
+++ stream.py
@@ -156,7 +156,7 @@ def main():
                 # 显示每个组合结果并附加行号
                 for idx, combo in enumerate(combinations, start=1):
                     t = custom_text(idx, combo)
-                    st.markdown('''''t''''', unsafe_allow_html=True)
+                    st.markdown('''t''', unsafe_allow_html=True)
                 
                 # HTML和JavaScript代码实现复制功能
                 copy_button_html = f"""
